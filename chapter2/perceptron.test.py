93cbdabdfa991d374d3c37cc35c883a7b519a420
masayuki.hirata <masayuki.h0729@gmail.com>
2016-12-24 13:58:06 +0900

    andとorを実装

---
+++ chapter2/perceptron.test.py
@@ -0,0 +1,21 @@
+import unittest
+
+import perceptron
+
+class TestStringMethods(unittest.TestCase):
+
+  def testAnd(self):
+    self.assertEqual(perceptron.AND(1,1), 1)
+    self.assertEqual(perceptron.AND(0,1), 0)
+    self.assertEqual(perceptron.AND(1,0), 0)
+    self.assertEqual(perceptron.AND(0,0), 0)
+
+  def testOr(self):
+    self.assertEqual(perceptron.OR(1,1), 1)
+    self.assertEqual(perceptron.OR(0,1), 1)
+    self.assertEqual(perceptron.OR(1,0), 1)
+    self.assertEqual(perceptron.OR(0,0), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
