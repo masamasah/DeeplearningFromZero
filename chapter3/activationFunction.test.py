35d92256a23a1d10adc56c6bef20c204918392ae
masayuki.hirata <masayuki.h0729@gmail.com>
2016-12-24 15:40:53 +0900

    softmax関数の実装

--- chapter3/activationFunction.test.py
+++ chapter3/activationFunction.test.py
@@ -6,6 +6,7 @@ import activationFunction
 
 class TestStringMethods(unittest.TestCase):
 
+
   def testStepFunction(self):
     self.assertEqual(activationFunction.step_function(np.array([1])), 1)
     self.assertEqual(activationFunction.step_function(np.array([0])), 0)
@@ -26,6 +27,13 @@ class TestStringMethods(unittest.TestCase):
     self.assertEqual(activationFunction.relu(5.0), 5.0)
     # just a little bit ugry...
     self.assertEqual(activationFunction.relu(np.array([-1.0, 0, 1.0, 5.0])).tolist(), np.array([0, 0, 1.0, 5.0]).tolist())
+
+  def testSoftmaxFunction(self):
+    val = activationFunction.softmax(np.array([0.3, 2.9, 4.0]))
+    roundedVal = [round(x, 4) for x in val.tolist()]
+    # just a little bit ugry...
+    self.assertEqual(roundedVal, np.array([ 0.0182, 0.2452, 0.7366]).tolist())
+
 
 if __name__ == '__main__':
     unittest.main()
