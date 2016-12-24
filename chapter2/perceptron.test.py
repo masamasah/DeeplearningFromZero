08843056dcdee295bd155cc71d9263d173943072
masayuki.hirata <masayuki.h0729@gmail.com>
2016-12-24 14:06:17 +0900

    NANDとXORを実装

--- chapter2/perceptron.test.py
+++ chapter2/perceptron.test.py
@@ -16,6 +16,17 @@ class TestStringMethods(unittest.TestCase):
     self.assertEqual(perceptron.OR(1,0), 1)
     self.assertEqual(perceptron.OR(0,0), 0)
 
+  def testNAND(self):
+    self.assertEqual(perceptron.NAND(1,1), 0)
+    self.assertEqual(perceptron.NAND(0,1), 1)
+    self.assertEqual(perceptron.NAND(1,0), 1)
+    self.assertEqual(perceptron.NAND(0,0), 1)
+
+  def testXOR(self):
+    self.assertEqual(perceptron.XOR(1,1), 0)
+    self.assertEqual(perceptron.XOR(0,1), 1)
+    self.assertEqual(perceptron.XOR(1,0), 1)
+    self.assertEqual(perceptron.XOR(0,0), 0)
 
 if __name__ == '__main__':
     unittest.main()
