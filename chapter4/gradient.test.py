dafb2d250a1b6878d21728bebdee9b35eb889411
masayuki.hirata <masayuki.h0729@gmail.com>
2017-01-02 13:08:02 +0900

    テストケースの追加

--- chapter4/gradient.test.py
+++ chapter4/gradient.test.py
@@ -10,6 +10,8 @@ class TestStringMethods(unittest.TestCase):
           return 0.01 * x ** 2 + 0.1 * x
 
       self.assertEqual(round(gradient.numerical_diff(func, 10), 4), 0.300)
+      self.assertEqual(round(gradient.numerical_diff(func, 0), 4), 0.100)
+      self.assertEqual(round(gradient.numerical_diff(func, -10), 4), -0.100)
 
 
 
