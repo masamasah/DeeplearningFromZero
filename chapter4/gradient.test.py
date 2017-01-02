66c8e57decbe759125ac427c32f95bfa6d046711
masayuki.hirata <masayuki.h0729@gmail.com>
2017-01-02 13:04:18 +0900

    微分の実装

---
+++ chapter4/gradient.test.py
@@ -0,0 +1,18 @@
+import unittest
+import numpy as np
+
+import gradient
+
+class TestStringMethods(unittest.TestCase):
+
+  def testMeanSquredError(self):
+      def func(x):
+          return 0.01 * x ** 2 + 0.1 * x
+
+      self.assertEqual(round(gradient.numerical_diff(func, 10), 4), 0.300)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
