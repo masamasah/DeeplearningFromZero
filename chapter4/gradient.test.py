d9c6a6bd498061fef6882fa324172bde8844c8d2
masayuki.hirata <masayuki.h0729@gmail.com>
2017-01-02 13:40:04 +0900

    偏微分を実装

--- chapter4/gradient.test.py
+++ chapter4/gradient.test.py
@@ -13,7 +13,21 @@ class TestStringMethods(unittest.TestCase):
       self.assertEqual(round(gradient.numerical_diff(func, 0), 4), 0.100)
       self.assertEqual(round(gradient.numerical_diff(func, -10), 4), -0.100)
 
+  def testNumericalGradient(self):
+      def func(x):
+          return x[0]**2 + x[1]**2
 
+      ans = gradient.numerical_gradient(func, np.array([3.0, 4.0]))
+      roundedVal = [round(x, 4) for x in ans.tolist()]
+      self.assertEqual(roundedVal, [6.0000, 8.0000])
+
+      ans = gradient.numerical_gradient(func, np.array([3.0, 0.0]))
+      roundedVal = [round(x, 4) for x in ans.tolist()]
+      self.assertEqual(roundedVal, [6.0000, 0.0000])
+
+      ans = gradient.numerical_gradient(func, np.array([0.0, 2.0]))
+      roundedVal = [round(x, 4) for x in ans.tolist()]
+      self.assertEqual(roundedVal, [0.0000, 4.0000])
 
 
 if __name__ == '__main__':
