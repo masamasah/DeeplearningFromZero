1481da60250e5ee4b6ed01d6bc868630e940ee5f
masayuki.hirata <masayuki.h0729@gmail.com>
2017-01-29 18:02:10 +0900

    AffineとSofmaxのレイヤーを追加

--- chapter5/Layers.py
+++ chapter5/Layers.py
@@ -1,3 +1,9 @@
+import sys, os
+
+sys.path.append(os.getcwd())
+from chapter3.activationFunction import softmax
+from chapter4.costFunctions import cross_entropy_error
+
 class MulLayer:
     def __init__(self):
         self.x = None
@@ -58,5 +64,44 @@ class Sigmoid:
 
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = sotfmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
 
         return dx
