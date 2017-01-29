464a933b5b8e868e0807da96942923bff2d165ad
masayuki.hirata <masayuki.h0729@gmail.com>
2017-01-29 17:20:50 +0900

    教科書のケースを追加

--- chapter5/Layers.test.py
+++ chapter5/Layers.test.py
@@ -59,6 +59,40 @@ class TestAddLayer(unittest.TestCase):
       self.assertEqual(dapple, 300)
       self.assertEqual(dorange, 300)
 
+class TestCompLayer(unittest.TestCase):
+
+  def testcomp(self):
+      apple = 100
+      apple_num = 2
+      orange = 150
+      orange_num = 3
+      tax = 1.1
+
+      mul_apple_layer = l.MulLayer()
+      mul_orange_layer = l.MulLayer()
+      add_price_layer = l.AddLayer()
+      mul_tax_layer = l.MulLayer()
+
+      apple_price = mul_apple_layer.forward(apple, apple_num)
+      orange_price = mul_orange_layer.forward(orange, orange_num)
+
+      price = add_price_layer.forward(apple_price, orange_price)
+
+      total_price = mul_tax_layer.forward(price, tax)
+
+      self.assertEqual(round(total_price,0), 715)
+
+      dprice = 1
+      dprice, dtax = mul_tax_layer.backward(1)
+      dapple_price, dorange_price = add_price_layer.backward(dprice)
+      dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+      dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+
+      self.assertEqual(round(dapple, 1), 2.2)
+      self.assertEqual(round(dapple_num, 0), 110)
+      self.assertEqual(round(dorange, 1), 3.3)
+      self.assertEqual(round(dorange_num, 0), 165)
+      self.assertEqual(round(dtax, 0), 650)
 
 if __name__ == '__main__':
     unittest.main()
